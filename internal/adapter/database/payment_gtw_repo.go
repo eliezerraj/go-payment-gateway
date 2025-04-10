9b53cf54c1295c3100f2916ad4b3b96eef759f1a
eliezerraj <eliezerraj@gmail.com>
2025-04-09 21:35:56 -0300

    feat(all):1st commit

---
+++ internal/adapter/database/payment_gtw_repo.go
@@ -0,0 +1,242 @@
+package database
+
+import (
+	"context"
+	"time"
+	"errors"
+
+	"github.com/go-payment-gateway/internal/core/model"
+	"github.com/go-payment-gateway/internal/core/erro"
+
+	go_core_observ "github.com/eliezerraj/go-core/observability"
+	go_core_pg "github.com/eliezerraj/go-core/database/pg"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/rs/zerolog/log"
+)
+
+var tracerProvider go_core_observ.TracerProvider
+var childLogger = log.With().Str("component","go-payment-gateway").Str("package","internal.adapter.database").Logger()
+
+type WorkerRepository struct {
+	DatabasePGServer *go_core_pg.DatabasePGServer
+}
+
+// Above new worker
+func NewWorkerRepository(databasePGServer *go_core_pg.DatabasePGServer) *WorkerRepository{
+	childLogger.Info().Str("func","NewWorkerRepository").Send()
+
+	return &WorkerRepository{
+		DatabasePGServer: databasePGServer,
+	}
+}
+
+// About add payment
+func (w *WorkerRepository) AddPayment(ctx context.Context, tx pgx.Tx, payment *model.Payment) (*model.Payment, error){
+	childLogger.Info().Str("func","AddPayment").Interface("trace-resquest-id", ctx.Value("trace-request-id")).Send()
+
+	// Trace
+	span := tracerProvider.Span(ctx, "database.AddPayment")
+	defer span.End()
+
+	// Prepare
+	payment.CreatedAt = time.Now()
+	if payment.PaymentAt.IsZero(){
+		payment.PaymentAt = payment.CreatedAt
+	}
+
+	// Query and execute
+	query := `INSERT INTO payment ( fk_card_id, 
+									card_number, 
+									fk_terminal_id, 
+									terminal, 
+									card_type, 
+									card_model, 
+									payment_at, 
+									mcc, 
+									status, 
+									currency, 
+									amount,
+									transaction_id,
+									request_id,
+									created_at,
+									tenant_id)
+				VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING id`
+
+	row := tx.QueryRow(ctx, query,  payment.FkCardId,
+									payment.CardNumber,
+									payment.FkTerminalId,
+									payment.Terminal,
+									payment.CardType,
+									payment.CardModel,
+									payment.PaymentAt,
+									payment.Mcc,
+									payment.Status,
+									payment.Currency,
+									payment.Amount,
+									payment.TransactionId,
+									payment.RequestId,
+									payment.CreatedAt ,
+									payment.TenantID)
+
+	var id int
+	if err := row.Scan(&id); err != nil {
+		return nil, errors.New(err.Error())
+	}
+
+	// set PK
+	payment.ID = id
+
+	return payment , nil
+}
+
+// About get terminal
+func (w *WorkerRepository) GetTerminal(ctx context.Context, terminal model.Terminal) (*model.Terminal, error){
+	childLogger.Info().Str("func","GetTerminal").Interface("trace-resquest-id", ctx.Value("trace-request-id")).Send()
+	
+	// Trace
+	span := tracerProvider.Span(ctx, "database.GetTerminal")
+	defer span.End()
+
+	// Get connection
+	conn, err := w.DatabasePGServer.Acquire(ctx)
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+	defer w.DatabasePGServer.Release(conn)
+
+	// prepare
+	res_terminal := model.Terminal{}
+
+	// query and execute
+	query :=  `SELECT 	id, 
+						name, 
+						coord_x, 
+						coord_y, 
+						status, 
+						created_at, 
+						updated_at
+				FROM terminal
+				WHERE name =$1`
+
+	rows, err := conn.Query(ctx, query, terminal.Name)
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan( 	&res_terminal.ID, 
+							&res_terminal.Name, 
+							&res_terminal.CoordX, 
+							&res_terminal.CoordY, 
+							&res_terminal.Status,
+							&res_terminal.CreatedAt,
+							&res_terminal.UpdatedAt,
+		)
+		if err != nil {
+			return nil, errors.New(err.Error())
+        }
+		return &res_terminal, nil
+	}
+	
+	return nil, erro.ErrNotFound
+}
+
+// About update payment
+func (w *WorkerRepository) UpdatePayment(ctx context.Context, tx pgx.Tx, payment model.Payment) (int64, error){
+	childLogger.Info().Str("func","UpdatePayment").Interface("trace-resquest-id", ctx.Value("trace-request-id")).Send()
+
+	// Trace
+	span := tracerProvider.Span(ctx, "database.UpdatePayment")
+	defer span.End()
+
+	// Query and execute
+	query := `UPDATE payment
+				set status = $2,
+					updated_at = $3
+				where id = $1`
+
+	row, err := tx.Exec(ctx, query,	payment.ID,
+									payment.Status,
+									time.Now())
+	if err != nil {
+		return 0, errors.New(err.Error())
+	}
+	return row.RowsAffected(), nil
+}
+//--------------------
+
+// About add pix transaction
+func (w *WorkerRepository) AddPixTransaction(ctx context.Context, tx pgx.Tx, pixTransaction model.PixTransaction) (*model.PixTransaction, error){
+	childLogger.Info().Str("func","AddPixTransaction").Interface("trace-resquest-id", ctx.Value("trace-request-id")).Send()
+
+	// Trace
+	span := tracerProvider.Span(ctx, "database.AddPixTransaction")
+	defer span.End()
+
+	// Prepare
+	if pixTransaction.CreatedAt.IsZero(){
+		pixTransaction.CreatedAt = time.Now()
+	}
+
+	// Query and execute
+	query := `INSERT INTO pix_transaction ( fk_account_id_from, 
+											account_id_from, 
+											fk_account_id_to, 
+											account_id_to, 
+											transaction_id, 
+											transaction_at, 
+											currency, 
+											amount, 
+											status, 
+											request_id,
+											created_at)
+				VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id`
+
+	row := tx.QueryRow(ctx, query,  pixTransaction.AccountFrom.ID,
+									pixTransaction.AccountFrom.AccountID,
+									pixTransaction.AccountTo.ID,
+									pixTransaction.AccountTo.AccountID,
+									pixTransaction.TransactionId,
+									pixTransaction.TransactionAt,		
+									pixTransaction.Currency,
+									pixTransaction.Amount,
+									pixTransaction.Status,
+									pixTransaction.RequestId,
+									pixTransaction.CreatedAt ,
+	)
+
+	var id int
+	if err := row.Scan(&id); err != nil {
+		return nil, errors.New(err.Error())
+	}
+
+	// set PK
+	pixTransaction.ID = id
+
+	return &pixTransaction , nil
+}
+
+// About update payment
+func (w *WorkerRepository) UpdatePixTransaction(ctx context.Context, tx pgx.Tx, pixTransaction model.PixTransaction) (int64, error){
+	childLogger.Info().Str("func","UpdatePixTransaction").Interface("trace-resquest-id", ctx.Value("trace-request-id")).Send()
+
+	// Trace
+	span := tracerProvider.Span(ctx, "database.UpdatePixTransaction")
+	defer span.End()
+
+	// Query and execute
+	query := `UPDATE pix_transaction
+				SET status = $2,
+					updated_at = $3
+				WHERE id = $1`
+
+	row, err := tx.Exec(ctx, query,	pixTransaction.ID,
+									pixTransaction.Status,
+									time.Now())
+	if err != nil {
+		return 0, errors.New(err.Error())
+	}
+	return row.RowsAffected(), nil
+}
---
+++ internal/infra/server/server.go
@@ -0,0 +1,130 @@
+package server
+
+import (
+	"time"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"os"
+	"os/signal"
+	"syscall"
+	"context"
+
+	"github.com/go-payment-gateway/internal/adapter/api"	
+	"github.com/go-payment-gateway/internal/core/model"
+	go_core_observ "github.com/eliezerraj/go-core/observability"  
+
+	"github.com/gorilla/mux"
+	"github.com/rs/zerolog/log"
+
+	"github.com/eliezerraj/go-core/middleware"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/contrib/propagators/aws/xray"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
+)
+
+var childLogger = log.With().Str("component","go-payment-gateway").Str("package","internal.infra.server").Logger()
+
+var core_middleware middleware.ToolsMiddleware
+var tracerProvider go_core_observ.TracerProvider
+var infoTrace go_core_observ.InfoTrace
+
+type HttpServer struct {
+	httpServer	*model.Server
+}
+
+func NewHttpAppServer(httpServer *model.Server) HttpServer {
+	childLogger.Info().Str("func","NewHttpAppServer").Send()
+	return HttpServer{httpServer: httpServer }
+}
+
+// About start http server
+func (h HttpServer) StartHttpAppServer(	ctx context.Context, 
+										httpRouters *api.HttpRouters,
+										appServer *model.AppServer) {
+	childLogger.Info().Str("func","StartHttpAppServer").Send()
+			
+	// ---------------------- OTEL ---------------
+	infoTrace.PodName = appServer.InfoPod.PodName
+	infoTrace.PodVersion = appServer.InfoPod.ApiVersion
+	infoTrace.ServiceType = "k8-workload"
+	infoTrace.Env = appServer.InfoPod.Env
+	infoTrace.AccountID = appServer.InfoPod.AccountID
+
+	tp := tracerProvider.NewTracerProvider(	ctx, 
+											appServer.ConfigOTEL, 
+											&infoTrace)
+
+	if tp != nil {
+		otel.SetTextMapPropagator(xray.Propagator{})
+		otel.SetTracerProvider(tp)
+	}
+	defer func() { 
+		if tp != nil {
+			err := tp.Shutdown(ctx)
+			if err != nil{
+				childLogger.Error().Err(err).Send()
+			}
+		}
+		childLogger.Info().Msg("stop done !!!")
+	}()
+	
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.Use(core_middleware.MiddleWareHandlerHeader)
+
+	myRouter.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+		childLogger.Debug().Msg("/")
+		json.NewEncoder(rw).Encode(appServer)
+	})
+
+	health := myRouter.Methods(http.MethodGet, http.MethodOptions).Subrouter()
+    health.HandleFunc("/health", httpRouters.Health)
+
+	live := myRouter.Methods(http.MethodGet, http.MethodOptions).Subrouter()
+    live.HandleFunc("/live", httpRouters.Live)
+
+	header := myRouter.Methods(http.MethodGet, http.MethodOptions).Subrouter()
+    header.HandleFunc("/header", httpRouters.Header)
+
+	myRouter.HandleFunc("/info", func(rw http.ResponseWriter, req *http.Request) {
+		childLogger.Info().Str("HandleFunc","/info").Send()
+
+		rw.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(appServer)
+	})
+	
+	addPayment := myRouter.Methods(http.MethodPost, http.MethodOptions).Subrouter()
+	addPayment.HandleFunc("/addPayment", core_middleware.MiddleWareErrorHandler(httpRouters.AddPayment))		
+	addPayment.Use(otelmux.Middleware("go-payment-gateway"))
+
+	pixTransaction := myRouter.Methods(http.MethodPost, http.MethodOptions).Subrouter()
+	pixTransaction.HandleFunc("/pixTransaction", core_middleware.MiddleWareErrorHandler(httpRouters.PixTransaction))		
+	pixTransaction.Use(otelmux.Middleware("go-payment-gateway"))
+
+	srv := http.Server{
+		Addr:         ":" +  strconv.Itoa(h.httpServer.Port),      	
+		Handler:      myRouter,                	          
+		ReadTimeout:  time.Duration(h.httpServer.ReadTimeout) * time.Second,   
+		WriteTimeout: time.Duration(h.httpServer.WriteTimeout) * time.Second,  
+		IdleTimeout:  time.Duration(h.httpServer.IdleTimeout) * time.Second, 
+	}
+
+	childLogger.Info().Str("Service Port", strconv.Itoa(h.httpServer.Port)).Send()
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			childLogger.Error().Err(err).Msg("canceling http mux server !!!")
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+	<-ch
+
+	if err := srv.Shutdown(ctx); err != nil && err != http.ErrServerClosed {
+		childLogger.Error().Err(err).Msg("warning dirty shutdown !!!")
+		return
+	}
+}
